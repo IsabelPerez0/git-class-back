92b314f41290a746361a292a71380bb1af14af11
Isabel <maria.perez1.mt@usa.edu.co>
2022-09-22 21:29:29 -0500

    [update] Added new aplication-prod file Addes new route to costume controller

--- src/main/java/com/grupo13/app/rents/CostumeController.java
+++ src/main/java/com/grupo13/app/rents/CostumeController.java
@@ -1,15 +1,21 @@
 package com.grupo13.app.rents;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/Costume")
 public class CostumeController {
     
-    @GetMapping()
-    public String sayHello(){
-        return "Hello";
+    @GetMapping("/all")
+    public String getCostume(){
+        return "costume 1, costume 2";
+    }
+
+    @PostMapping("/save")
+    public String createCostume(){
+        return "Created ...";
     }
 }
